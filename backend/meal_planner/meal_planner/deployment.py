046d171c750e51d8834c47b426b912dca484607f
SonnyZhan <fumingzhang@umass.edu>
2025-04-30 21:01:16 -0400

    fixed typo and connection_str issues

--- backend/meal_planner/meal_planner/deployment.py
+++ backend/meal_planner/meal_planner/deployment.py
@@ -2,26 +2,26 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
-
+# Hosts & security
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', '')]
+CSRF_TRUSTED_ORIGINS = [f"https://{ALLOWED_HOSTS[0]}"]
 DEBUG = False
-
 SECRET_KEY = os.environ['MY_SECRET_KEY']
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.midddleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
+# Static files
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
@@ -30,18 +30,20 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-CONNECTION = os.environ["AZURE_POSTGRESQL_CONNECTIONSTRING"]
-CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split('')}
+# Parse the Azure Postgres connection string (format: key1=val1;key2=val2;…)
+raw = os.environ.get("AZURE_POSTGRESQL_CONNECTIONSTRING", "")
+pairs = [p for p in raw.split(";") if p and "=" in p]
+CONNECTION_STR = dict(pair.split("=", 1) for pair in pairs)
 
+# Database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backend.postgresql",
-        "Name" : CONNECTION_STR["dbname"],
-        "HOST" : CONNECTION_STR["host"],
-        "USER" : CONNECTION_STR["user"],
-        "PASSWORD" : CONNECTION_STR["password"],
+        "ENGINE": "django.db.backends.postgresql",        # backends (plural)
+        "NAME": CONNECTION_STR.get("dbname", ""),         # uppercase NAME
+        "HOST": CONNECTION_STR.get("host", ""),
+        "USER": CONNECTION_STR.get("user", ""),
+        "PASSWORD": CONNECTION_STR.get("password", ""),
     }
 }
-
-STATIC_ROOT = BASE_DIR/'staticfiles' #put all the staticfile into the base folder
